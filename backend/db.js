3796363ce19fab15d6d6a7362a2296495e4dd107
LearnRK <learn.rk10101@gmail.com>
2024-07-06 22:17:59 +0530

    a simple paytm clone

---
+++ backend/db.js
@@ -0,0 +1,58 @@
+// require mongoose
+const { kMaxLength } = require("buffer");
+const mongoose = require("mongoose");
+
+// connect to MongoDB
+mongoose
+.connect("mongodb://localhost:27017/paytm_1")
+.then(console.log("Connected to DB successfully."))
+.catch((e) =>{console.log("connection to DB failed.")})
+
+// create schema
+const UserSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    first_name:{
+        type:String,
+        required:true,
+        trim: true,
+        maxLength: 50
+    },
+    last_name:{
+        type:String,
+        required:true,
+        trim: true,
+        maxLength: 50
+    },
+    email:{
+        type:String,
+        required:false
+    },
+    phone_number:{
+        type:Number,
+        required:false
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength: 6
+    },
+    ammount:{
+        type:String,
+    },
+})
+
+// create a model of schema
+const User = mongoose.model('User',UserSchema);
+
+// export schema
+module.exports = {
+    User,
+}
